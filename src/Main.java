d3bc9d045b68c4619490bae7af5a7c8316c90219
Sebastian M Zdroana <s.zdroana@gmail.com>
2018-02-17 14:06:31 +0200

    branching test

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello World! din partea lui Sebi");
+        System.out.println("Hello World!");
 
-        System.out.println("This is a sad program.");
+        System.out.println("Can we print a tree?");
+        System.out.println("Yes we can");
 
         System.out.print("Please enter a number");
         int num = scanner.nextInt();
 
-        for (int i = 0; i < num; i++) {
-            System.out.println("Said what? " + scanner.next());
+        for (int i = 1; i < num + 1; i++) {
+            if (i < num + 1) {
+                for (int j = 1; j < i + 1; ++j) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        //num = 3      i = 3
+        for (int i = num + 1; i > 0; --i) {
+            for (int j = 1; j > num + 1; --j) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
 
     }
