a802c146199de54a8dc4e03d3b7601f206509203
Sebastian M Zdroana <s.zdroana@gmail.com>
2018-02-17 12:31:39 +0200

    Added input and forloop - 17/02/2017:12:31

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello World! din partea lui Sebi");
+
+        System.out.println("This is a sad program.");
+
+        System.out.print("Please enter a number");
+        int num = scanner.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("This is a sad program...");
+        }
+
     }
 }
